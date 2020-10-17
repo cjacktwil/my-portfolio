4e38a818223f3ae1d5d69a92db6ccc7f467bcc04
Tracey Jackson <62776009+cjacktwil@users.noreply.github.com>
2020-10-16 20:27:24 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <header>Tracey Jackson
+        <ul>
+          <li>About Me</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+          <li>Resume</li>
+        </ul>
+    </header>
+    <main> 
+      Content
+    </main>
+    <footer>
+      Github/LinkedIn info
+    </footer>
+  )
 }
 
 export default App;
