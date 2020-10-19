65b2ef09fcd47aa339e57ecfde5e334d783a6ce0
Tracey Jackson <62776009+cjacktwil@users.noreply.github.com>
2020-10-18 21:28:40 -0500

    Updates

---
+++ precache-manifest.9eacfd4dbdc40f73641bf1f1a1dfdbf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97075b92eb87cd8d03b8ef6062ee5c5",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "6106291fe576237a1e0d",
+    "url": "/my-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "51ebcd3eddfedf4a8da4",
+    "url": "/my-portfolio/static/js/2.245237e4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/my-portfolio/static/js/2.245237e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6106291fe576237a1e0d",
+    "url": "/my-portfolio/static/js/main.ae6d0a9c.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  }
+]);
