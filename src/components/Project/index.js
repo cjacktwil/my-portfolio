09827792c4789557326592626ac4f9262e3f40d7
Tracey Jackson <62776009+cjacktwil@users.noreply.github.com>
2021-01-05 10:57:34 -0600

    updating master with local changes

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -7,42 +7,42 @@ function Project() {
             languages: "Javascript, HTML, CSS",
             github: "https://github.com/taylornyquist/spontaneous-adventurer",
             link: "https://taylornyquist.github.io/spontaneous-adventurer/",
-            image: "../assets/images/screenshots/spontaneous-adventurer.png"
+            image: "./assets/images/screenshots/spontaneous-adventurer.png"
         },
         {
             name: "Buy Smart",
             languages: "JavaScript, CSS, Handlebars, Jquery Mobile, Sequelize, MySQL2, Express",
             github: "https://github.com/Alex2055/Buysmart",
             link: "https://fast-bastion-33726.herokuapp.com/",
-            image: "../public/assets/images/screenshots/buy-smart.png"
+            image: "./assets/images/screenshots/buy-smart.png"
         },
         {
             name: "Uncode Your Career",
             languages: "JavaScript, React, Express, GraphQL",
             github: "https://github.com/cjacktwil/uncode-your-career",
             link: "https://murmuring-everglades-03231.herokuapp.com/",
-            image: "../assets/images/screenshots/uncode-career.png"
+            image: "./assets/images/screenshots/uncode-career.png"
         },
         {
             name: "Run Buddy",
             languages: "HTML, CSS",
             github: "https://github.com/cjacktwil/run-buddy",
             link: "https://cjacktwil.github.io/run-buddy/",
-            image: "../assets/images/screenshots/run-buddy.png"
+            image: "./assets/images/screenshots/run-buddy.png"
         },
         {
             name: "Budget Tracker",
             languages: "JavaScript, CSS, Express, Mongoose",
             github: "https://github.com/cjacktwil/budget-tracker",
             link: "https://ancient-waters-23996.herokuapp.com/",
-            image: "../assets/images/screenshots/budget-tracker.png"
+            image: "./assets/images/screenshots/budget-tracker.png"
         },
         {
             name: "More Tech Blog",
             languages: "JavaScript, Handlebars, CSS, Express, Sequelize, MySQL2",
             github: "https://github.com/cjacktwil/more-tech",
             link: "https://lit-springs-36043.herokuapp.com/",
-            image: "../assets/images/screenshots/more-tech.png"
+            image: "./assets/images/screenshots/more-tech.png"
         }
 
     ])
